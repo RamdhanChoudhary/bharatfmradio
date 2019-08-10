b942ab4f5471f142cf1d93eb9b1282eededb5ffd
RDC <ramdhan.cse@gmail.com>
2019-08-10 17:37:50 +0530

    App Icons added for all the sizes Table View added for fm list

--- Bharat FM Radio/Bharat FM Radio/ViewController.swift
+++ Bharat FM Radio/Bharat FM Radio/ViewController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var fmListTableView: UITableView!
+
+    var fmListArry:[String] = ["Bhart FM Radio", "Vividh Bharti", "All India Radio" ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fmListArry.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = (self.fmListTableView.dequeueReusableCell(withIdentifier: "FMListCell") as UITableViewCell?)!
+        cell.selectionStyle = .none
+        cell.textLabel?.text = self.fmListArry[indexPath.row]
+        cell.textLabel?.textAlignment = .center
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Play FM Radio")
     }
 
     
